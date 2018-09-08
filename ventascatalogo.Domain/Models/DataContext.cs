8c3e4502d20f0df3db3d545b14f31d18494d49fd
juan.pablo.rios8 <juan.pablo.rios8@gmail.com>
2018-09-08 12:33:35 -0500

    arquitectura

---
+++ ventascatalogo.Domain/Models/DataContext.cs
@@ -0,0 +1,21 @@
+﻿namespace ventascatalogo.Domain.Models 
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DataContext : DbContext
+    {
+        #region Contructores
+        //cada vez que se instancie esta clase se conecta automaticamente a la base de datos
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+        #endregion
+
+    }
+}
